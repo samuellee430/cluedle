7a658f39e9a0de6e9dd9974d10e347da5af47499
Samuel <samuellee430@gmail.com>
2022-02-20 00:07:34 -0600

    fix issue where count was incorrect for duplicates

--- src/lib/counts.ts
+++ src/lib/counts.ts
@@ -6,43 +6,17 @@ export const getCounts = (guess: string): number => {
   const splitSolution = solution.split('')
   const splitGuess = guess.split('')
 
-  const solutionCharsTaken = splitSolution.map((_) => false)
-
-  // const statuses: CharStatus[] = Array.from(Array(guess.length))
   let count: number = 0
 
   // handle all correct cases first
   splitGuess.forEach((letter, i) => {
     if (letter === splitSolution[i]) {
-      // statuses[i] = 'correct'
       count += 2
-      solutionCharsTaken[i] = true
+      splitSolution[i] = ''
       return
-    }
-  })
-
-  splitGuess.forEach((letter, i) => {
-    // if (statuses[i]) return
-
-    if (!splitSolution.includes(letter)) {
-      // handles the absent case
-      // statuses[i] = 'absent'
-      return
-    }
-
-    // now we are left with "present"s
-    const indexOfPresentChar = splitSolution.findIndex(
-      (x, index) => x === letter && !solutionCharsTaken[index]
-    )
-
-    if (indexOfPresentChar > -1) {
-      // statuses[i] = 'present'
-      solutionCharsTaken[indexOfPresentChar] = true
+    } else if (splitSolution.indexOf(letter) > -1) {
       count += 1
-      return
-    } else {
-      // statuses[i] = 'absent'
-      return
+      splitSolution[splitSolution.indexOf(letter)] = ''
     }
   })
 
